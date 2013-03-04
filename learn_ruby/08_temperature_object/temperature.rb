ef36a7f7072f8d629dc6bdee8323b540425853fb
DrTr <dmytromuravskyi@gmail.com>
2013-03-04 19:57:32 +0200

    Order of functions changed

--- learn_ruby/08_temperature_object/temperature.rb
+++ learn_ruby/08_temperature_object/temperature.rb
@@ -3,6 +3,22 @@ class Temperature
     @data = options 
   end
   
+  def in_fahrenheit
+    t = @data.has_key?(:f) ? @data[:f] : ctof(@data[:c]) 
+  end
+
+  def in_celsius
+    t = @data.has_key?(:c) ? @data[:c] : ftoc(@data[:f])   
+  end
+  
+  def self.from_celsius(degree)
+    class_eval "Temperature.new(:c => #{degree})"
+  end
+  
+  def self.from_fahrenheit(degree)
+    class_eval "Temperature.new(:f => #{degree})"
+  end 
+  
   private
   def ctof(t)
     if t > 36 || t < 42
@@ -19,23 +35,6 @@ class Temperature
       (t - 32) * 5 / 9
     end
   end
-  
-  public
-  def in_fahrenheit
-    t = @data.has_key?(:f) ? @data[:f] : ctof(@data[:c]) 
-  end
-
-  def in_celsius
-    t = @data.has_key?(:c) ? @data[:c] : ftoc(@data[:f])   
-  end
-  
-  def self.from_celsius(degree)
-    class_eval "Temperature.new(:c => #{degree})"
-  end
-  
-  def self.from_fahrenheit(degree)
-    class_eval "Temperature.new(:f => #{degree})"
-  end 
 end
 
 class Celsius < Temperature
@@ -51,7 +50,7 @@ class Celsius < Temperature
     @data
   end
 end
-
+  
 class Fahrenheit < Temperature
     def initialize(degree)
     @data = degree
